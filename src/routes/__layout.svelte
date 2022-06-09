1414ba7b193f49ca986e07bfc55c41eb03b484ba
Kyle Shepherd <kyle@thisissoon.com>
2022-06-09 15:34:26 +0100

    feat(Layout): Base styling

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,17 @@
   :global {
     @import "../styles/global.scss";
   }
+
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: $light;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 </style>
 
-<slot />
+<main>
+  <slot />
+</main>

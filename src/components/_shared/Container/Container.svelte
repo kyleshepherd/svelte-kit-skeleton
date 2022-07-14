742b7cec834e7b9da51094489d18557b41360cdd
Kyle Shepherd <kyle@thisissoon.com>
2022-07-14 15:05:26 +0100

    feat(Container): Create component

---
+++ src/components/_shared/Container/Container.svelte
@@ -0,0 +1,20 @@
+<style lang="scss">
+  div {
+    max-width: 100%;
+    width: 1024px;
+    padding: 0 1rem;
+    margin: 0 auto;
+
+    @include breakpoint(lg) {
+      width: 1280px;
+    }
+
+    @include breakpoint(xl) {
+      width: 1440px;
+    }
+  }
+</style>
+
+<div {...$$props}>
+  <slot />
+</div>
